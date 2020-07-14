31d0d39985d0d4d76f299870e925f90c17e53049
BrunoNguyen2905 <nhannguyen290520@gmail.com>
2020-07-14 15:57:02 +0300

    fixing delete product bug

--- models/cart.js
+++ models/cart.js
@@ -44,6 +44,9 @@ module.exports = class Cart {
            }
            const updatedCart = {...JSON.parse(fileContent)};
            const product = updatedCart.products.find(prod => prod.id === id);  //find product id to get info abt product.qty
+            if(!product){ // if product dont exist,means cart dont have that product, just return and dont continue remaining code
+                return;
+            }
            const productQty = product.qty;
            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
            updatedCart.totalPrice = updatedCart.totalPrice - productPrice*productQty;
--- models/product.js
+++ models/product.js
@@ -52,7 +52,7 @@ module.exports = class Product {
             const updatedProducts = products.filter(prod => prod.id !== id);
             fs.writeFile(p, JSON.stringify(updatedProducts), err => {
                 if(!err) {
-                    Cart.deleteProduct(id, product.price);
+                    Cart.deleteProduct(id, product.price); //delete product no longer available in cart
                 }
             })
         });
